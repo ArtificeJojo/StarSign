891df67ff3fd92fab2759100a7c1b94863a8612d
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-07-11 17:57:40 -0400

    Web Post
    Added new logic for Web Post.
    Added Skybox.

---
+++ Assets/Scripts/WebPost1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebPost1 : MonoBehaviour
+{
+    private WebReq webReq;
+    private string txt = "Cancer";
+
+    public static string id;
+    //private string Url;
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        StartCoroutine(PostReq("http://172.20.10.4:5000/zodiac", txt));
+    }
+
+    public static IEnumerator PostReq(string url, string data)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("zodiac", data);
+
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+        Debug.Log("Result: " + request.result);
+        
+        yield return request.SendWebRequest();
+        id = request.downloadHandler.text;
+        Debug.Log(id);   
+    }
+}
