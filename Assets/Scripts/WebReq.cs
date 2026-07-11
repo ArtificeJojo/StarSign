25b3e1d9a37b99a7a34b37581aac544586c53f48
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-07-11 12:07:30 -0400

    Web Request
    Able to access a web url and play audio

--- Assets/Scripts/WebReq.cs
+++ Assets/Scripts/WebReq.cs
@@ -1,26 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class WebReq : MonoBehaviour
 {
     //public string Url;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetReq("http://10.32.241.86:5000/"));
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(GetReq("http://172.20.10.4:5000/"));
     }
     
     IEnumerator GetReq(string url)
     {
-        UnityWebRequest request = UnityWebRequest.Get(url);
+        //using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
+        UnityWebRequest request = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.WAV);
         yield return request.SendWebRequest();
         
         if (request.isNetworkError || request.isHttpError)
             Debug.Log("Error: " + request.error);
         else
-            Debug.Log("Successfully Received:" + request.downloadHandler.text);
+        {
+            Debug.Log("Successfully Received:" + request.downloadHandler + url);
+            AudioClip audioClip = DownloadHandlerAudioClip.GetContent(request);
+            if (audioClip != null)
+            {
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                Debug.Log("Audio Played");
+                /*
+                string save = Path.Combine(Application.dataPath, "audioClip.wav");
+                File.WriteAllBytes(save, request.downloadHandler.data);
+                Debug.Log("Audio Saved to " + save);
+                */
+            }
+            else
+                Debug.Log("No Audio Found or Download Error");
+            
+        }
+            
             
         
     }
