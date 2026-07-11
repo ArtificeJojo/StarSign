91f27da003f1d6969318a84a64fb9aaf29c45e9b
Mr-PainTheSecond <santiagosanhuezaw@gmail.com>
2026-07-11 11:52:04 -0400

    Some prompts added. .env.example added

--- Flask/app.py
+++ Flask/app.py
@@ -1,14 +1,26 @@
 from flask import Flask
+from google import genai
+import os
+from dotenv import load_dotenv
+from werkzeug.middleware.proxy_fix import ProxyFix
 from gemini_data import GeminiData
 
 
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route("/", methods = ["GET"])
 def test():
     contents = None
-    with open("MU_sound_check") as file:
+    with open("MU_sound_check_ignore.wav", "rb+") as file:
         contents = file.read()
     
     return contents
+
+@app.route("/ai", methods= ["POST"])
+def audio_test():
+    
+    load_dotenv()
+    client = genai.Client(api_key=os.getenv("GEMINI_API"))
+    return ""
--- Flask/gemini_data.py
+++ Flask/gemini_data.py
@@ -1,13 +1,13 @@
 class GeminiData:
     
     star_signs = {
-    "Aries": "They are charismatic and honest with their feelings, living for the thrills and to lead other. But they are bad at reading the room and don't know their own limits",
-    "Taurus": "They like to be ",
-    "Gemini": "",
-    "Cancer": "",
-    "Virgo": "",
-    "Leo": "",
-    "Libra": "",
+    "Aries": "They are charismatic and honest with their feelings, living for the thrills and to lead other. But they are bad at reading the room and don't know their own limits.",
+    "Taurus": "They like to be slow and certain, and like to be secure in all aspect of life. They struggle with flexibility and accepting change.",
+    "Gemini": "They are explorers who like have a wide variety of experiences and friends, highly adaptable and tackling life with excitement. However, they tend to have commitement issues, and",
+    "Cancer": "They love deeply and are very honest, and expect others to do the same. However, they are very suspicious of others and can be easily hurt, making it harder for them to be vulnarable.",
+    "Leo": "They are very caring and generous, with no problem helping other. However, they have quite the ego and always want to be the center of attention.",
+    "Virgo": "They are high achievers, highly self-aware, and great at actings. However, they take everything too seriously and never know when to loosen up and have fun.",
+    "Libra": "They are very kind, being very empathetic, but a big fence-sitter, unwilling to stand up and speak their mind.",
     "Scorpio": "",
     "Sagittarius": "",
     "Capricorn": "",
