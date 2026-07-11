c38ecee3625f2bd4ed26fa5adf02920f39a96d8a
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-07-11 11:01:18 -0400

    Level Design & Web Request
    Started Working on Level Design and WebRequest script

--- Assets/Scripts/WebReq.cs
+++ Assets/Scripts/WebReq.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class WebReq : MonoBehaviour
 {
+    //public string Url;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GetReq("http://10.32.241.86:5000/"));
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    IEnumerator GetReq(string url)
     {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log("Error: " + request.error);
+        else
+            Debug.Log("Successfully Received:" + request.downloadHandler.text);
+            
         
     }
 }
